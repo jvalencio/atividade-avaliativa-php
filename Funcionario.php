9448329372386844ddd5185c14a2b367eeb23ce1
João Valencio <jooaovs23@gmail.com>
2026-09-10 18:11:28 -0300

    feat: add employee class

---
+++ Funcionario.php
@@ -0,0 +1,57 @@
+<?php
+
+class Funcionario {
+    private string $nome;
+    private string $cargo;
+    private float $salario;
+
+    public function __construct(string $nome, string $cargo, float $salario) {
+        $this->nome = $nome;
+        $this->cargo = $cargo;
+        $this->salario = $salario;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function getCargo(): string {
+        return $this->cargo;
+    }
+
+    public function getSalario(): float {
+        return $this->salario;
+    }
+
+    public function setSalario(float $valor): void {
+        if ($valor > 0) {
+            $this->salario = $valor;
+        }
+    }
+
+    public function setCargo(string $novoCargo): void {
+        $cargosAceitos = ["Analista", "Assistente", "Coordenador"];
+
+        if (in_array($novoCargo, $cargosAceitos)) {
+            $this->cargo = $novoCargo;
+        }
+    }
+
+    public function calcularSalarioLiquido(): float {
+        $salarioLiquido = $this->salario * 0.89; // desconto de 11%
+
+        if ($salario > 5000) {
+            return $salarioLiquido * 0.95; // desconto adicional de 5%
+        }
+
+        return $salarioLiquido;
+    }
+
+    public function ehElegivelParaBonus(): bool {
+        if ($this->cargo == "Coordenador" && $this->calcularSalarioLiquido() > 4000) {
+            return true;
+        }
+
+        return false;
+    }
+}
