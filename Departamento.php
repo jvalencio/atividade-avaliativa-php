97a97f35fd81b20f7981300124cea1d9502eb3bc
João Valencio <jooaovs23@gmail.com>
2026-09-10 20:26:58 -0300

    feat: add department class

---
+++ Departamento.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once "Funcionario.php";
+
+class Departamento {
+    private array $funcionarios = [];
+
+    public function adicionarFuncionario(Funcionario $f): void {
+        $this->funcionarios[] = $f;
+    }
+
+    public function calcularFolhaTotal(): float {
+        $folhaTotal = 0;
+
+        foreach ($this->funcionarios as $f) {
+            $folhaTotal += $f->calcularSalarioLiquido();
+        }
+
+        return $folhaTotal;
+    }
+
+    public function funcionariosPorCargo(string $cargo): array {
+        $funcionariosDoCargo = [];
+
+        foreach ($this->funcionarios as $f) {
+            if ($f->getCargo() == $cargo) {
+                $funcionariosDoCargo[] = $f;
+            }
+        }
+
+        return $funcionariosDoCargo;
+    }
+
+    public function mediaSalarial(): float {
+        if (count($funcionarios) > 0)
+            return $this->calcularFolhaTotal() / count($funcionarios);
+
+        return 0.0;
+    }
+}
